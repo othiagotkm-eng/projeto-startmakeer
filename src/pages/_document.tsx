28b8bc0c4783800e5851e3ebeff88dc1100f31cd
Leonardo Lima Dorea <leolimadorea@gmail.com>
2025-01-10 09:02:20 -0300

    hotjar test

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -73,21 +73,6 @@ export default function Document() {
             alt="Novo Meta Pixel"
           />
         </noscript>
-        {/* Hotjar */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(c,s,q,u,a,r,e){
-                c.hj=c.hj||function(){(c.hj.q=c.hj.q||[]).push(arguments)};
-                c._hjSettings={hjid:5262179};
-                r=s.getElementsByTagName('head')[0];
-                e=s.createElement('script');e.async=true;
-                e.src=q+c._hjSettings.hjid+u;
-                r.appendChild(e);
-              })(window,document,'https://static.hj.contentsquare.net/csq-','.js', 5262179);
-            `,
-          }}
-        />
       </Head>
       <body>
         <Main />
